a53784d20f54eb2ac00d797654abbc11c94bda85
Raees453 <raeesali453@gmail.com>
2026-03-15 05:29:05 +0500

    fix server host for railway

--- server/index.ts
+++ server/index.ts
@@ -86,7 +86,7 @@ app.use((req, res, next) => {
   }
 
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, "127.0.0.1", () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://127.0.0.1:${PORT}`);
   });
 })();
